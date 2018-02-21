809cc4d59a7657284d8b9a8bee0a4aa8e57a8b28
Jeff Liekens <liekensjeff@gmail.com>
2018-02-21 15:08:40 +0100

    Order finished todo’s bubble sort by completedDate

--- Todo/src/pages/todos-done/todos-done.ts
+++ Todo/src/pages/todos-done/todos-done.ts
@@ -19,8 +19,25 @@ export class TodosDonePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public todoDataProv: TodoDataProvider) {
     this.todoDataProv.getTodosDone().then((todos) => {
-      this.todos = todos;
+      this.todos = this.sortArrayByDate(todos);
     });
+  }
+
+
+  sortArrayByDate(todos: Array<Todo>) {
+    var length = todos.length;
+    //Bubble sort
+    for(let i = 1; i < length;i++) {
+      for(var j = 0;j < length - i;j++) {
+        if(todos[j].completedAt < todos[j+1].completedAt){
+          // Swap
+          var temp = todos[j];
+          todos[j] = todos[j+1];
+          todos[j + 1] = temp;
+        }
+      }
+    }
+    return todos;
   }
 
 
