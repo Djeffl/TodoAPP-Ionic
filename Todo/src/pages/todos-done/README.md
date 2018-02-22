29e558fb674839d13f5b935611ab90e0537776da
Jeff Liekens <liekensjeff@gmail.com>
2018-02-22 14:20:05 +0100

    Sort finished Todos per day functionality & display

--- Todo/src/pages/todos-done/README.md
+++ Todo/src/pages/todos-done/README.md
@@ -1,5 +1,5 @@
 # TodosDonePage
 
-1. Sorteer op datum & tijd de finished items, (laatste gefinished bovenaan)
-2. Splits finished items per dag en zet er een header elke keer bovenaan met dag
+1. Sorteer op datum & tijd de finished items, (laatste gefinished bovenaan) - done
+2. Splits finished items per dag en zet er een header elke keer bovenaan met dag - done
 3. Undo button, verplaats terug naar actieve todos
--- Todo/src/pages/todos-done/todos-done.ts
+++ Todo/src/pages/todos-done/todos-done.ts
@@ -15,11 +15,11 @@ import { Todo } from '../../models/todo';
   templateUrl: 'todos-done.html',
 })
 export class TodosDonePage {
-  todos: Array<Todo> = [];
+  displayTodos: Array<{ date: string, todos: Array<Todo>}> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public todoDataProv: TodoDataProvider) {
     this.todoDataProv.getTodosDone().then((todos) => {
-      this.todos = this.sortArrayByDate(todos);
+      this.displayTodos = this.convertToDisplayTodos(todos);
     });
   }
 
@@ -40,5 +40,81 @@ export class TodosDonePage {
     return todos;
   }
 
+  convertToDisplayTodos(todos: Array<Todo>) {
+    todos = this.sortArrayByDate(todos);
+    // Create displayDate
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+    var nextTodoHasSameDate: boolean;
+    var currentTodoDate: Date;
+    var nextTodoDate: Date;
+    var displayDate: string;
+    var todosgroup: Array<Todo> = [];
+    var index = 0;
+    var displayTodos: Array<{date: string, todos: Array<Todo>}> = [];
 
+    while(index < todos.length) {
+      // Reset todosgroup
+      todosgroup = [];
+      // Get current todo
+      var currentTodo: Todo = todos[index];
+      // Get todo completion date
+      currentTodoDate = currentTodo.completedAt;
+
+      // Compare the days/year/month with eachother
+      var currentTodoDay = currentTodoDate.getDate();
+      var currentTodoMonth = currentTodoDate.getMonth();
+      var currentTodoYear = currentTodoDate.getFullYear();
+
+      // Compose date to display in html (eg: 24 August 2017)
+      displayDate = currentTodoDay + " " + monthNames[currentTodoMonth] + " " + currentTodoYear;
+
+      // Next todo variables
+      var nextTodo: Todo;
+      var nextTodoDay;
+      var nextTodoMonth;
+      var nextTodoYear;
+
+      // Push currentTodo to group Date todos
+      todosgroup.push(currentTodo);
+
+      // While same date loop
+      do {
+        // Check if last todo or not
+        if (index < todos.length - 1){
+          // Get next todo completion date
+          nextTodo      =  todos[index+1];
+          nextTodoDate  =  nextTodo.completedAt;
+          nextTodoDay   =  nextTodoDate.getDate();
+          nextTodoMonth =  nextTodoDate.getMonth();
+          nextTodoYear  =  nextTodoDate.getFullYear();
+
+          // if match add to list of todos of currentDate
+          if(currentTodoYear == nextTodoYear && currentTodoMonth == nextTodoMonth && currentTodoDay == nextTodoDay) {
+            //
+            todosgroup.push(nextTodo);
+            nextTodoHasSameDate = true;
+          }
+          // If not add todos list to displayTodos as group with currentDate
+          else {
+            displayTodos.push({ date: displayDate, todos: todosgroup });
+            nextTodoHasSameDate = false;
+          }
+        }
+        // If last item push it into list
+        else {
+          displayTodos.push({ date: displayDate, todos: todosgroup });
+          nextTodoHasSameDate = false;
+        }
+        index++;
+      }
+      while(nextTodoHasSameDate);
+    }
+    displayTodos.push({date: "26 December 2017", todos: [{name: "P", time: "20:00"}]})
+    return displayTodos;
+  }
 }
