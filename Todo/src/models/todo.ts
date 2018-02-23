8cb0c9f5ab388670b1bb058065e0cf2d9c299516
Jeff Liekens <liekensjeff@gmail.com>
2018-02-23 11:26:58 +0100

    Add different constructors to Todo’s

--- Todo/src/models/todo.ts
+++ Todo/src/models/todo.ts
@@ -1,21 +1,43 @@
 export class Todo {
   id: string; //UUID
   name: string;
-  time: string;
+  time: string; // Remove me soon please
   done: boolean;
   completedAt: Date;
   createdAt: Date;
-  details: any;
-  //type: string; // Options: [none, subtasks, time] Note to self: visualize with icons & fancy shizzle
-  //startTime: string;
-  //subTasks: any;
-  //timeLeft: string;
+  type: string; // Options: [none, subtasks, time] Note to self: visualize with icons & fancy shizzle
+  startTime: string;
+  subTasks: Array<string>;
+  timeLeft: string;
 
-  constructor(id: string, name:string, time: string, done: boolean, createdAt: Date) {
+  // Basic type
+  constructor(id: string, type: string, name:string, done: boolean, createdAt: Date) {
     this.id = id;
     this.name = name;
     this.time = time;
     this.done = done;
     this.createdAt = createdAt;
+  }
+
+  // Time type
+  constructor(id: string, type: string, name: string, done: boolean, createdAt: Date, startTime: string) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.done = done;
+    this.createdAt = createdAt;
+    this.startTime = startTime;
+    this.timeLeft = startTime;
+  }
+
+  // Assignments type
+  constructor(id: string, type: string, name: string, done: boolean, createdAt: Date, subTasks: Array<string>) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.done = done;
+    this.createdAt = createdAt;
+    this.startTime = startTime;
+    this.timeLeft = startTime;
   }
 }
